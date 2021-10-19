5097b97a3383fb196df120b59c1f5d03ffded31f
Lisa Cormier <lcormier5@hotmail.com>
2021-10-19 23:39:27 +0000

    add spec for add to cart button

---
+++ spec/features/add_to_cart_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.feature "AddToCarts", type: :feature, js: true do
+  # SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+  end
+
+  scenario "They see product features" do
+    # ACT
+    visit root_path
+
+    page.find('footer').find_button('Add').click
+
+    page.has_content?('My Cart (1)')
+
+    # DEBUG / VERIFY
+    #save_screenshot
+
+  end
+end
